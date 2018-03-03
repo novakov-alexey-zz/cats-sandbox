51ba0dcb5438d1c5d6118a08f4b8fca4c5dc7d4c
Alexey Novakov <an@datawerks.com>
2018-03-03 19:29:51 +0100

    save progress on Functors

--- src/main/scala/sandbox/functor/Functors.scala
+++ src/main/scala/sandbox/functor/Functors.scala
@@ -61,3 +61,44 @@ object Functors_ex2 extends  App {
   println(format(Box("hello world")))
   //format(Box(123))
 }
+
+trait Codec[A] { self =>
+  def encode(value: A): String
+  def decode(value: String): A
+  def imap[B](dec: A => B, enc: B => A): Codec[B] = new Codec[B] {
+    override def encode(value: B): String = self.encode(enc(value))
+
+    override def decode(value: String): B = dec(self.decode(value))
+  }
+}
+object Codec {
+  def encode[A](value: A)(implicit c: Codec[A]): String = c.encode(value)
+  def decode[A](value: String)(implicit c: Codec[A]): A = c.decode(value)
+}
+
+object Codecs extends App {
+  import Codec._
+
+  implicit val stringCodec: Codec[String] =
+    new Codec[String] {
+      def encode(value: String): String = value
+      def decode(value: String): String = value
+    }
+
+  implicit val doubleCodec: Codec[Double] =
+    stringCodec.imap(_.toDouble, _.toString)
+
+//  println(encode(10.2d))
+  val d = decode[Double]("10.2d")
+//  println(d)
+
+  implicit def boxCodec[A](implicit c: Codec[A]): Codec[Box[A]] = new Codec[Box[A]] {
+    override def encode(value: Box[A]): String = c.encode(value.value)
+
+    override def decode(value: String): Box[A] = Box(c.decode(value))
+  }
+
+  val box = encode(Box(1.2))
+  println(box)
+  println(decode[Box[Double]]("1.2"))
+}
